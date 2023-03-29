d61a41d275b1a2d9071494bed4df18e6471ae1fc
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-03-29 19:51:32 +0400

    toolbar buttons

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -4,7 +4,19 @@ import "../styles/toolbar.scss";
 interface Props {}
 
 const Toolbar = (props: Props) => {
-  return <div className="toolbar">Toolbar</div>;
+  return (
+    <div className="toolbar">
+      <button className="toolbar__btn brush" />
+      <button className="toolbar__btn rect" />
+      <button className="toolbar__btn circle" />
+      <button className="toolbar__btn eraser" />
+      <button className="toolbar__btn line" />
+      <input type="color" style={{ marginLeft: "10px" }} />
+      <button className="toolbar__btn undo" />
+      <button className="toolbar__btn redo" />
+      <button className="toolbar__btn save" />
+    </div>
+  );
 };
 
 export default Toolbar;
