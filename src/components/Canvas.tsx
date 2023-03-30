fd64d9954ff19f33b9778625b8b5387a0275b7c4
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-03-30 23:30:01 +0400

    New tool rect works in canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useRef } from "react";
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
 import "../styles/canvas.scss";
+import Brush from "../tools/Brush";
 
 interface Props {}
 
-const Canvas = (props: Props) => {
+const Canvas = observer((props: Props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current);
+    toolState.setTool(
+      new Brush(canvasRef.current as HTMLCanvasElement, null, 0)
+    );
+  }, []);
+
   return (
     <div className="canvas">
-      <canvas width={600} height={400} />
+      <canvas ref={canvasRef} width={600} height={400} />
     </div>
   );
-};
+});
 
 export default Canvas;
---
+++ src/tools/Tool.ts
@@ -0,0 +1,44 @@
+import { MouseEvent } from "react";
+export default class Tool {
+  canvas: HTMLCanvasElement | null;
+  socket: any;
+  id: number;
+  ctx: CanvasRenderingContext2D | null;
+  startX: number;
+  startY: number;
+  width: number;
+  height: number;
+  mouseDown: boolean;
+  saved: string;
+
+  constructor(canvas: HTMLCanvasElement, socket: any, id: number) {
+    this.canvas = canvas;
+    this.socket = socket;
+    this.id = id;
+    this.ctx = canvas.getContext("2d");
+    this.destroyEvents();
+    this.startX = 0;
+    this.startY = 0;
+    this.width = 0;
+    this.height = 0;
+    this.mouseDown = false;
+    this.saved = "";
+  }
+
+  set fillColor(color: string) {
+    this.ctx!.fillStyle = color;
+  }
+  set strokeColor(color: string) {
+    this.ctx!.strokeStyle = color;
+  }
+
+  set lineWidth(width: number) {
+    this.ctx!.lineWidth = width;
+  }
+
+  destroyEvents() {
+    this.canvas!.onmousemove = null;
+    this.canvas!.onmousedown = null;
+    this.canvas!.onmouseup = null;
+  }
+}
